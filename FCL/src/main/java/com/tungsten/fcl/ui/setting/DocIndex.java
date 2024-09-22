13882b21a9903b20e42f237a5910b37d393d1837
hyplant <hy18918868804@163.com>
2024-09-22 15:16:55 +0800

    fix

--- FCL/src/main/java/com/tungsten/fcl/ui/setting/DocIndex.java
+++ FCL/src/main/java/com/tungsten/fcl/ui/setting/DocIndex.java
@@ -38,7 +38,7 @@ public class DocIndex {
 
     public String getDisplayName(Context context) {
         for (DisplayName name : displayName) {
-            if (name.getLang() == null || LocaleUtils.getLocale(LocaleUtils.getLanguage(context)).toString().contains(name.getLang()) {
+            if (name.getLang() == null || LocaleUtils.getLocale(LocaleUtils.getLanguage(context)).toString().contains(name.getLang())) {
                 return name.getName();
             }
         }
