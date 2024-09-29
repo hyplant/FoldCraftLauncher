52b4ea41b9d7055a4334062705c61d86d4cb252a
hyplant <hy18918868804@163.com>
2024-09-29 20:22:52 +0800

    fix

--- FCL/src/main/java/com/tungsten/fcl/activity/MainActivity.kt
+++ FCL/src/main/java/com/tungsten/fcl/activity/MainActivity.kt
@@ -544,7 +544,7 @@ class MainActivity : FCLActivity(), OnSelectListener, View.OnClickListener {
             isOutsideTouchable = true
             enterTransition = Slide(Gravity.TOP)
             exitTransition = Slide(Gravity.TOP)
-            showAsDropDown(launchPojav)
+            showAsDropDown(bind.launchPojav)
         }
     }
 
