5bbbc33a8076bde7e172c40c0c5a67caaa6386f5
hyplant <hy18918868804@163.com>
2024-11-17 12:00:57 +0800

    还原文件

--- FCL/src/main/java/com/tungsten/fcl/ui/main/MainUI.java
+++ FCL/src/main/java/com/tungsten/fcl/ui/main/MainUI.java
@@ -42,13 +42,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
 
 public class MainUI extends FCLCommonUI implements View.OnClickListener {
-
-    public static final String ANNOUNCEMENT_URL = FCLApplication.appConfig.getProperty("announcement-url","https://raw.githubusercontent.com/hyplant-team/FoldCraftLauncher/doc/announcement/latest.json");
+  
+    public static final String ANNOUNCEMENT_URL = FCLApplication.appConfig.getProperty("announcement-url","https://icraft.ren:90/titles/FCL/Releases_Version/1.1.8.4/announcement.txt");
 
     private LinearLayoutCompat announcementContainer;
     private LinearLayoutCompat announcementLayout;
@@ -56,11 +55,6 @@ public class MainUI extends FCLCommonUI implements View.OnClickListener {
     private FCLTextView announcementView;
     private FCLTextView date;
     private FCLButton hide;
-
-    private FCLButton buttonp;
-    private FCLButton buttonr;
-    private FCLButton buttons;
-
     private Announcement announcement = null;
 
     private RelativeLayout skinContainer;
@@ -83,14 +77,8 @@ public class MainUI extends FCLCommonUI implements View.OnClickListener {
         announcementView = findViewById(R.id.announcement);
         date = findViewById(R.id.date);
         hide = findViewById(R.id.hide);
-        buttonp = findViewById(R.id.buttonp);
-        buttonr = findViewById(R.id.buttonr);
-        buttons = findViewById(R.id.buttons);
         ThemeEngine.getInstance().registerEvent(announcementLayout, () -> announcementLayout.getBackground().setTint(ThemeEngine.getInstance().getTheme().getColor()));
         hide.setOnClickListener(this);
-        buttonp.setOnClickListener(this);
-        buttonr.setOnClickListener(this);
-        buttons.setOnClickListener(this);
 
         skinContainer = findViewById(R.id.skin_container);
         renderer = new SkinRenderer(getContext());
@@ -156,58 +144,37 @@ public class MainUI extends FCLCommonUI implements View.OnClickListener {
     }
 
     private void checkAnnouncement() {
-        announcementContainer.setVisibility(View.GONE);
-        AtomicReference<String> remoteDataRef = new AtomicReference<>();
-        AtomicReference<Announcement> announcementDataRef = new AtomicReference<>();
         if(FCLApplication.appConfig.getProperty("enable-announcement","true").equals("true")){
-            title.setText(getContext().getString(R.string.announcement));
-            announcementView.setText(getContext().getString(R.string.announcement_loading));
-            date.setText(new String(ANNOUNCEMENT_URL));
-            announcementContainer.setVisibility(View.VISIBLE);
-            CompletableFuture<Announcement> future = CompletableFuture.supplyAsync(() -> {
+            @SuppressLint("SimpleDateFormat") CompletableFuture<Announcement> future = CompletableFuture.supplyAsync(() -> {
                 try {
-                    String remoteData = NetworkUtils.doGet(NetworkUtils.toURL(ANNOUNCEMENT_URL), FCLApplication.deviceInfoUtils.toString());
-                    remoteDataRef.set(remoteData);
+                    return new Gson().fromJson(NetworkUtils.doGet(NetworkUtils.toURL(ANNOUNCEMENT_URL), FCLApplication.deviceInfoUtils.toString()), Announcement.class);
                 }catch (Exception e) {
-                    e.printStackTrace();
                     return new Announcement(
-                        -1, true, false, -1, -1, new ArrayList<>(),
-                        new ArrayList<>(Collections.singletonList(new Announcement.Content(null, getContext().getString(R.string.announcement_error_network)))),
-                        new String(ANNOUNCEMENT_URL),
-                        new ArrayList<>(Collections.singletonList(new Announcement.Content(null, getContext().getString(R.string.announcement_error_network_content) + "\n" + ANNOUNCEMENT_URL)))
+                            -1,
+                            true,
+                            false,
+                            -1,
+                            -1,
+                            new ArrayList<>(),
+                            new ArrayList<>(Collections.singletonList(new Announcement.Content("en", "异常"))),
+                            new SimpleDateFormat("yyyy.MM.dd").format(new Date()),
+                            new ArrayList<>(Collections.singletonList(new Announcement.Content("en", "无法获取公告，原因：无效的公告地址或JSON文件格式无效")))
                     );
                 }
-                try {
-                    Announcement announcementData = new Gson().fromJson(remoteDataRef.get(), Announcement.class);
-                    announcementDataRef.set(announcementData);
-                }catch (Exception e) {
-                    e.printStackTrace();
-                    return new Announcement(
-                        -1, true, false, -1, -1, new ArrayList<>(),
-                        new ArrayList<>(Collections.singletonList(new Announcement.Content(null, getContext().getString(R.string.announcement_error_format)))),
-                        new String(ANNOUNCEMENT_URL),
-                        new ArrayList<>(Collections.singletonList(new Announcement.Content(null, getContext().getString(R.string.announcement_error_format_content) + "\n" + remoteDataRef.get())))
-                    );
-                }
-                return announcementDataRef.get();
             });
             future.thenAccept(announcement -> new Handler(Looper.getMainLooper()).post(() -> {
                 this.announcement = announcement;
                 try {
-                    if (!announcement.shouldDisplay(getContext())) {
-                        announcementContainer.setVisibility(View.GONE);
-                        return;
-                    }
-                    title.setText(this.announcement.getDisplayTitle(getContext()));
+                    title.setText(AndroidUtils.getLocalizedText(getContext(), "announcement", this.announcement.getDisplayTitle(getContext())));
                     announcementView.setText(this.announcement.getDisplayContent(getContext()));
-                    date.setText(this.announcement.getDate());
+                    date.setText(AndroidUtils.getLocalizedText(getContext(), "update_date", this.announcement.getDate()));
                 }catch(Exception e) {
-                    title.setText(getContext().getString(R.string.announcement_error_data));
-                    announcementView.setText(ANNOUNCEMENT_URL);
-                    date.setText(getContext().getString(R.string.announcement_error_data_content) + "\n" + remoteDataRef.get());
+                    title.setText("异常");
+                    announcementView.setText("无法获取公告，原因：无效的JSON文件格式");
+                    date.setText(new SimpleDateFormat("yyyy.MM.dd").format(new Date()));
                 }
             }));
-        }
+        }else announcementContainer.setVisibility(View.GONE);
     }
 
     private void hideAnnouncement() {
@@ -244,42 +211,19 @@ public class MainUI extends FCLCommonUI implements View.OnClickListener {
     }
 
     @Override
-    public void onBackPressed() {
-        checkAnnouncement();
-        FCLAlertDialog.Builder builder = new FCLAlertDialog.Builder(getContext());
-        builder.setAlertLevel(FCLAlertDialog.AlertLevel.INFO);
-        builder.setCancelable(false);
-        builder.setMessage(getContext().getString(R.string.menu_settings_force_exit_msg));
-        builder.setPositiveButton(getContext().getString(com.tungsten.fcllibrary.R.string.dialog_negative), null);
-        builder.setNegativeButton(getContext().getString(com.tungsten.fcllibrary.R.string.dialog_positive), () -> {
-            getActivity().finish();
-            System.exit(0);
-        });
-        builder.create().show();
-    }
-
-    @Override
     public void onClick(View view) {
         if (view == hide) {
             if (announcement != null && announcement.isSignificant()) {
                 FCLAlertDialog.Builder builder = new FCLAlertDialog.Builder(getContext());
                 builder.setAlertLevel(FCLAlertDialog.AlertLevel.ALERT);
-                builder.setCancelable(true);
+                builder.setCancelable(false);
                 builder.setMessage(getContext().getString(R.string.announcement_significant));
-                builder.setPositiveButton(null, null);
-                builder.setNegativeButton(getContext().getString(com.tungsten.fcllibrary.R.string.dialog_positive), null);
+                builder.setPositiveButton(this::hideAnnouncement);
+                builder.setNegativeButton(null);
                 builder.create().show();
             } else {
                 hideAnnouncement();
             }
-        } else if (view == buttonp) {
-            onPause();
-        }
-        } else if (view == buttonr) {
-            onResume();
-        }
-        } else if (view == buttons) {
-            onStop();
         }
     }
 }
