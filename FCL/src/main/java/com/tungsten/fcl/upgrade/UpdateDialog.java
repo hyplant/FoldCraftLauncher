d49bbc2f5adc709e58cf825d0a02c0a471368937
hyplant <hy18918868804@163.com>
2024-11-23 14:48:28 +0800

    import ClipboardManager

--- FCL/src/main/java/com/tungsten/fcl/upgrade/UpdateDialog.java
+++ FCL/src/main/java/com/tungsten/fcl/upgrade/UpdateDialog.java
@@ -1,5 +1,7 @@
 package com.tungsten.fcl.upgrade;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Point;
