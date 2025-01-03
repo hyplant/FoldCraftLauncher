080891dcbb0b67f0c39fe6ffc406245900bb7641
hyplant <hy18918868804@163.com>
2025-01-03 12:55:16 +0800

    update assets

--- FCL/src/main/java/com/tungsten/fcl/fragment/RuntimeFragment.kt
+++ FCL/src/main/java/com/tungsten/fcl/fragment/RuntimeFragment.kt
@@ -86,7 +86,7 @@ class RuntimeFragment : FCLFragment(), View.OnClickListener {
             java21 = RuntimeUtils.isLatest(FCLPath.JAVA_21_PATH, "/assets/app_runtime/java/jre21")
             jna = RuntimeUtils.isLatest(FCLPath.JNA_PATH, "/assets/app_runtime/jna")
             gameResource = RuntimeUtils.isLatest(FCLPath.SHARED_COMMON_DIR, "/assets/.minecraft")
-            others = RuntimeUtils.isLatest(FCLPath.FILES_DIR, "/assets/othersInternal/files")
+            others = RuntimeUtils.isLatest(FCLPath.INTERNAL_DIR, "/assets/othersInternal")
         } catch (e: IOException) {
             e.printStackTrace()
         }
